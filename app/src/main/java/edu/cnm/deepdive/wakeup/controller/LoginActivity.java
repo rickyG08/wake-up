0f5884a20610c35cefa9265b14648e8766df1df7
Ricky Garcia <rickyg08@msn.com>
2020-11-22 09:17:15 -0700

    Started GoogleSignInService

---
+++ app/src/main/java/edu/cnm/deepdive/wakeup/controller/LoginActivity.java
@@ -0,0 +1,14 @@
+package edu.cnm.deepdive.wakeup.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import edu.cnm.deepdive.wakeup.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_login);
+  }
+}
