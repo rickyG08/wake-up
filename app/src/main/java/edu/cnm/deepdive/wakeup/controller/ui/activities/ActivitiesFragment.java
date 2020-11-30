1239d27d2d779a9da83acca2c834165221970d6d
Ricky Garcia <rickyg08@msn.com>
2020-11-30 08:12:31 -0700

    Adding new launcher icon

--- app/src/main/java/edu/cnm/deepdive/wakeup/controller/ui/activities/ActivitiesFragment.java
+++ app/src/main/java/edu/cnm/deepdive/wakeup/controller/ui/activities/ActivitiesFragment.java
@@ -9,6 +9,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 import edu.cnm.deepdive.wakeup.R;
 import edu.cnm.deepdive.wakeup.databinding.FragmentActivitiesBinding;
@@ -16,19 +17,12 @@ import edu.cnm.deepdive.wakeup.databinding.FragmentActivitiesBinding;
 public class ActivitiesFragment extends Fragment {
 
   private ActivitiesViewModel activitiesViewModel;
+  private FragmentActivitiesBinding binding;
 
   public View onCreateView(@NonNull LayoutInflater inflater,
       ViewGroup container, Bundle savedInstanceState) {
-    activitiesViewModel =
-        ViewModelProviders.of(this).get(ActivitiesViewModel.class);
-    View root = inflater.inflate(R.layout.fragment_activities, container, false);
-    final TextView textView = root.findViewById(R.id.activities_list_view);
-    activitiesViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-      @Override
-      public void onChanged(@Nullable String s) {
-        textView.setText(s);
-      }
-    });
-    return root;
+    binding = FragmentActivitiesBinding.inflate(inflater);
+    activitiesViewModel = new ViewModelProvider(this).get(ActivitiesViewModel.class);
+    return binding.getRoot();
   }
 }
