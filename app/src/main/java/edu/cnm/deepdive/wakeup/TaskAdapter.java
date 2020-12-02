54ca5151b225fbe933644c1b3c2671322b246e5e
Ricky Garcia <rickyg08@msn.com>
2020-12-02 11:56:52 -0700

    Working on adding tasks to list

---
+++ app/src/main/java/edu/cnm/deepdive/wakeup/TaskAdapter.java
@@ -0,0 +1,62 @@
+package edu.cnm.deepdive.wakeup;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import edu.cnm.deepdive.wakeup.TaskAdapter.Holder;
+import edu.cnm.deepdive.wakeup.databinding.FragmentTodoBinding;
+import edu.cnm.deepdive.wakeup.model.entity.Todo;
+import java.util.List;
+
+public class TaskAdapter extends RecyclerView.Adapter<Holder> {
+
+  private final Context context;
+  private final LayoutInflater inflater;
+  private final List<Todo> tasks;
+  private final OnClickListener listener;
+
+  public TaskAdapter(Context context, List<Todo> tasks,
+      OnClickListener listener) {
+    this.context = context;
+    this.tasks = tasks;
+    inflater = LayoutInflater.from(context);
+    this.listener = listener;
+  }
+
+
+  @NonNull
+  @Override
+  public Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    FragmentTodoBinding binding = FragmentTodoBinding.inflate(inflater, parent, false);
+    return new Holder(binding);
+  }
+
+  @Override
+  public void onBindViewHolder(@NonNull Holder holder, int position) {
+
+  }
+
+  @Override
+  public int getItemCount() {
+    return 0;
+  }
+
+  public class Holder extends RecyclerView.ViewHolder {
+
+    private final FragmentTodoBinding binding;
+
+    public Holder(FragmentTodoBinding binding) {
+      super(binding.getRoot());
+      this.binding = binding;
+    }
+
+    private void bind(int position) {
+      Todo todo = tasks.get(position);
+      bind.
+    }
+  }
+
+}
--- app/src/main/java/edu/cnm/deepdive/wakeup/controller/ui/todo/TodoFragment.java
+++ app/src/main/java/edu/cnm/deepdive/wakeup/controller/ui/todo/TodoFragment.java
@@ -1,28 +1,64 @@
 package edu.cnm.deepdive.wakeup.controller.ui.todo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
 import edu.cnm.deepdive.wakeup.R;
+import edu.cnm.deepdive.wakeup.controller.MainActivity;
 import edu.cnm.deepdive.wakeup.databinding.FragmentTodoBinding;
 
 public class TodoFragment extends Fragment {
 
   private TodoViewModel todoViewModel;
   private FragmentTodoBinding binding;
+  private Button addTask;
+  private EditText taskName;
+  private EditText taskDescription;
+  private EditText taskDate;
+  private RecyclerView taskItemList;
 
   public View onCreateView(@NonNull LayoutInflater inflater,
       ViewGroup container, Bundle savedInstanceState) {
     binding = FragmentTodoBinding.inflate(inflater);
     todoViewModel = new ViewModelProvider(this).get(TodoViewModel.class);
     return binding.getRoot();
+  }
+
+  @Override
+  public void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    addTask = getActivity().findViewById(R.id.add_button);
+    taskName = getActivity().findViewById(R.id.task_name);
+    taskDescription = getActivity().findViewById(R.id.task_description);
+    taskDate = getActivity().findViewById(R.id.task_date);
+    taskItemList = getActivity().findViewById(R.id.task_list_item);
+
+    addTask.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        if (!taskName.getText().toString().isEmpty() && !taskDescription.getText().toString()
+            .isEmpty() && !taskDate.getText().toString().isEmpty()) {
+
+          Intent intent = new Intent(Intent.ACTION_INSERT);
+
+        } else {
+//          Toast.makeText(v, "Fill all fields.", Toast.LENGTH_LONG).show();
+        }
+      }
+    });
   }
 }
